6a9c818905142655d8db416d586ed6f8ff81c927
CTZDev <carlosteranzavaleta@hotmail.com>
2021-07-19 02:31:00 -0500

    Toggle Active Done!

---
+++ dist/js/modules/toggle.js
@@ -0,0 +1,26 @@
+const d = document;
+
+export default function toggle() {
+  const $toggle = d.getElementById("toggle");
+  const $toggleButton = d.getElementById("toggleButton");
+  const $themes = d.querySelectorAll(".navigation-theme-values > span");
+
+  d.addEventListener("click", (e) => {
+    if (e.target === $toggle) {
+      let widthToggle = $toggle.clientWidth;
+      let toggleSplit = widthToggle / $themes.length;
+      let x = e.offsetX;
+
+      if (x >= 0 && x < toggleSplit) {
+        $toggleButton.classList.remove("theme2");
+        $toggleButton.classList.remove("theme3");
+      } else if (x >= toggleSplit && x < toggleSplit * 2) {
+        $toggleButton.classList.add("theme2");
+        $toggleButton.classList.remove("theme3");
+      } else {
+        $toggleButton.classList.remove("theme2");
+        $toggleButton.classList.add("theme3");
+      }
+    }
+  });
+}
---
+++ dist/js/script.js
@@ -0,0 +1,5 @@
+import toggle from "./modules/toggle.js";
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  toggle();
+});
